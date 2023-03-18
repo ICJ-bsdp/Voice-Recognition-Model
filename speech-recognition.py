64bbb462122bb1a6990f8d3af973409ef82e9674
maxx06 <67605840+maxx06@users.noreply.github.com>
2023-03-18 00:25:46 -0400

    multilanguage handling w/ semifinished input-output translation

--- speech-recognition.py
+++ speech-recognition.py
@@ -3,28 +3,28 @@ import azure.cognitiveservices.speech as speechsdk
 import time
 
 def recognize_from_microphone():
-    speech_config = speechsdk.SpeechConfig(subscription=('ca5c981b5b02433883bcb941aecb7e03'), region=('eastus'))
-    speech_config.speech_recognition_language="zh-CN" #en-US  zh-CN es-ES
-    speech_config.set_profanity(speechsdk.ProfanityOption.Raw)
+    speech_translation_config = speechsdk.translation.SpeechTranslationConfig(subscription='6c1f18d17acb4e4d84c4dc228d560c3b', region='eastus')
+    speech_translation_config.speech_recognition_language="en-US"
+
+    target_language="it"
+    speech_translation_config.add_target_language(target_language)
+
+    auto_detect_source_language_config = speechsdk.languageconfig.AutoDetectSourceLanguageConfig(languages=["en-US", "zh-CN", "ko-KR", "es-ES"])
 
     audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
-    
+    translation_recognizer = speechsdk.translation.TranslationRecognizer(translation_config=speech_translation_config, audio_config=audio_config, auto_detect_source_language_config=auto_detect_source_language_config)
+
     print("Speak into your microphone.")
-    
-    # for i in range(10):
-    #     time.sleep(0.5)
-    #     speech_recognition_result = speech_recognizer.().get()
-    #     print("Recognized: {}".format(speech_recognition_result.text))
-
-    speech_recognition_result = speech_recognizer.recognize_once_async().get()
-
-    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(speech_recognition_result.text))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_recognition_result.cancellation_details
+    translation_recognition_result = translation_recognizer.recognize_once_async().get()
+    if translation_recognition_result.reason == speechsdk.ResultReason.TranslatedSpeech:
+        print("Recognized: {}".format(translation_recognition_result.text))
+        print("""Translated into '{}': {}""".format(
+            target_language, 
+            translation_recognition_result.translations[target_language]))
+    elif translation_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(translation_recognition_result.no_match_details))
+    elif translation_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = translation_recognition_result.cancellation_details
         print("Speech Recognition canceled: {}".format(cancellation_details.reason))
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
             print("Error details: {}".format(cancellation_details.error_details))
